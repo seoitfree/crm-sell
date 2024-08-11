854d03c4f1d7c647eb4e816bb3038197c0fbb06a
Vlad Yakovenko <botvot33@gmail.com>
2024-08-11 10:31:31 +0300

    [CRM-SELL-0] Deleted vee-validate from LogIn form;

--- resources/js/src/pages/Login/LoginForm.vue
+++ resources/js/src/pages/Login/LoginForm.vue
@@ -1,18 +1,17 @@
 
 <template>
-    <Form tag="form" ref='form' class="auth-form login-form" v-slot="{ handleSubmit }">
+    <form tag="form" ref="form" class="auth-form login-form">
         <div class="email mb-3 text-center">
             <label class="sr-only" for="signin-email">Email</label>
-            <Field name="signin-email" type="signin-email" class="form-control signin-email" placeholder="Email" :rules="validation.email" v-slot="{ errors }" v-model="form.email"></Field>
-            <ErrorMessage name="signin-email" class="text-danger" />
+            <input name="email" type="email" class="form-control signin-email"  placeholder="Email" v-model="form.email">
+            <span v-if="'email' in errors" role="alert" class="text-danger" >{{ errors.email }}</span>
             <span class="text-danger" v-if="errorServerValidation !== ''">{{ errorServerValidation }}</span>
         </div>
 
         <div class="password mb-3 text-center">
-
             <label class="sr-only" for="signin-password">Password</label>
-            <Field id="signin-password" name="signin-password" type="password" class="form-control signin-password" placeholder="Password" :rules="validation.password" v-slot="{ errors }" v-model="form.password"></Field>
-            <ErrorMessage name="signin-password" class="text-danger" />
+            <input name="password" type="password" class="form-control signin-password"  placeholder="Password" v-model="form.password">
+            <span v-if="'password' in errors" role="alert" class="text-danger" >{{ errors.password }}</span>
 
             <div class="extra mt-3 row justify-content-between">
                 <div class="col-6">
@@ -32,14 +31,13 @@
         </div>
 
         <div v-if="!processing" class="text-center">
-            <button type="button" class="btn app-btn-primary w-100 theme-btn mx-auto" @click="handleSubmit($event, onSubmit)">Log In</button>
+            <button type="submit" class="btn app-btn-primary w-100 theme-btn mx-auto" @click="onSubmit">Log In</button>
         </div>
         <button v-if="processing" class="btn btn-primary text-center" type="button" disabled>
             <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
             <span class="visually-hidden">Loading...</span>
         </button>
-
-    </Form>
+    </form>
 </template>
 
 <script lang="ts">
@@ -47,19 +45,12 @@
 import { defineComponent, ref } from 'vue';
 import axios from "axios";
 import { useUserStore } from "../../stores/UserStore";
-import {UserInfo} from "../../types/userInfo";
-import { Form, Field, ErrorMessage} from 'vee-validate';
 import * as yup from 'yup';
 import {initAuth} from "../../modules/Auth/auth.service";
 const userStore = useUserStore();
 
 export default defineComponent({
     name: "LoginForm",
-    components: {
-        Form,
-        Field,
-        ErrorMessage
-    },
     data() {
         return {
             processing: false,
@@ -68,19 +59,40 @@ export default defineComponent({
                 email: '' as string,
                 password: '' as string,
             },
-            validation: {
-                email:  yup.string().required('Поле обзательное').email('Некоректный формат.').min(8, 'Минимальное количество символов 8'),
-                password: yup.string().required('Поле обзательное').min(8, 'Минимальное количество символов 8'),
-            }
+            validation: yup.object().shape({
+                email:  yup.string().required('Поле обзательное')
+                    .email('Некоректный формат.')
+                    .min(8, 'Минимальное количество символов 8'),
+                password: yup.string()
+                    .required('Поле обзательное')
+                    .min(8, 'Минимальное количество символов 8'),
+            }),
+            errors: {},
         }
     },
     methods: {
-        async onSubmit(values): void {
+        onSubmit(e) {
+            e.preventDefault();
+            this.errors = {};
+            const schema = this.validation;
+            schema.validate(this.form, { abortEarly: false })
+                .then(valid => this.initLogin())
+                .catch(errors => {
+                    const errorsObject = {};
+                    errors.inner.forEach(err => {
+                        errorsObject[err.path] = err.message;
+                    });
+                    this.errors = errorsObject;
+                });
+        },
+        async initLogin(): void {
             this.processing = true;
             this.errorServerValidation = '';
             axios.get('/sanctum/csrf-cookie').then((response) => {
                 this.login();
-            }).catch(() => {
+            }).catch((error) => {
+                console.error(error);
+                alert("Ошбка сервера, перегрузите страницу или обратитесь в тех поддержку.");
                 this.processing = false;
             });
         },
@@ -98,10 +110,12 @@ export default defineComponent({
                 if (response.status === 422) {
                     this.errorServerValidation = 'Некоректный email или пароль.'
                     this.processing = false;
-                } else {
-                    this.getUserData(this.getXsrfToken());
+                    return;
                 }
+                this.getUserData(this.getXsrfToken());
             }).catch((error) => {
+                console.error(error)
+                alert("Ошбка сервера, перегрузите страницу или обратитесь в тех поддержку.");
                 this.processing = false;
             });
         },
